bb3502f05cb99e490a0b76aaace35c1d7e541cd4
Nik Berezovsky <unikolas@macbook-pro.home>
2021-01-16 18:48:11 +0000

    Catch me game

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,35 +1,20 @@
-const COLOR_BLACK = '#15171A'
-const COLOR_WHITE = '#FFFFFF'
-
-const COLOR_PRIMARY = '#0666EB'
-const COLOR_ACCENT = '#E950A4'
-
-const COLOR_ERROR = '#F54C3E'
-const COLOR_SUCCESS = '#23A423'
-const COLOR_WARNING = '#F7930D'
-
-const COLOR_GREY_35 = '#525C66'
-const COLOR_GREY_50 = '#828B94'
-const COLOR_GREY_80 = '#C0C6CC'
-const COLOR_GREY_90 = '#DFE2E6'
-const COLOR_GREY_95 = '#EDEEF0'
-const COLOR_GREY_100 = '#F6F7F8'
-
 const colors = {
-    black: COLOR_BLACK,
-    white: COLOR_WHITE, 
+    black: '#15171A',
+    white: '#FFFFFF', 
 
-    primary: COLOR_PRIMARY,
-    acccent: COLOR_ACCENT,
+    primary: '#0666EB',
+    acccent: '#E950A4',
 
-    error: COLOR_ERROR,
-    warning: COLOR_WARNING,
-    success: COLOR_SUCCESS,
+    error: '#F54C3E',
+    warning: '#23A423',
+    success: '#F7930D',
 
-    grey35: COLOR_GREY_35,
-    grey50: COLOR_GREY_50,
-    grey80: COLOR_GREY_80,
-    grey100: COLOR_GREY_100,
+    grey35: '#525C66',
+    grey50: '#828B94',
+    grey80: '#C0C6CC',
+    grey90: '#DFE2E6',
+    grey95: '#EDEEF0',
+    grey100: '#F6F7F8',
 }
 
 export default colors
